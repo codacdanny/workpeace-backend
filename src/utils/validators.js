abd703a7de482aa3fd8063fb9150190735d3278f
Chinemerem Daniel <danpresh302@gmail.com>
2025-05-17 23:24:00 +0100

    fix update data names for response body

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,27 +1,25 @@
 import { body } from "express-validator";
 
 export const instructorFormValidation = [
-  body("firstName").trim().notEmpty().withMessage("First name is required"),
-  body("lastName").trim().notEmpty().withMessage("Last name is required"),
-  body("workEmail").isEmail().withMessage("Valid work email is required"),
-  body("mobileNumber").notEmpty().withMessage("Mobile number is required"),
-  body("courseExpertise")
-    .notEmpty()
-    .withMessage("Course expertise is required"),
+  body("firstname").trim().notEmpty().withMessage("First name is required"),
+  body("lastname").trim().notEmpty().withMessage("Last name is required"),
+  body("email").isEmail().withMessage("Valid work email is required"),
+  body("mobile").notEmpty().withMessage("Mobile number is required"),
+  body("course").notEmpty().withMessage("Course expertise is required"),
   body("occupation").notEmpty().withMessage("Occupation is required"),
-  body("reason").notEmpty().withMessage("Reason to join is required"),
+  body("reasonToJoinUs").notEmpty().withMessage("Reason to join is required"),
 ];
 
 export const contactFormValidation = [
-  body("fullName").trim().notEmpty().withMessage("Full name is required"),
-  body("emailAddress").isEmail().withMessage("Valid email address is required"),
-  body("mobileNumber").notEmpty().withMessage("Mobile number is required"),
+  body("fullname").trim().notEmpty().withMessage("Full name is required"),
+  body("email").isEmail().withMessage("Valid email address is required"),
+  body("mobile").notEmpty().withMessage("Mobile number is required"),
   body("message").notEmpty().withMessage("Message is required"),
 ];
 
 export const hiringFormValidation = [
-  body("firstName").trim().notEmpty().withMessage("First name is required"),
-  body("lastName").trim().notEmpty().withMessage("Last name is required"),
-  body("workEmail").isEmail().withMessage("Valid work email is required"),
-  body("mobileNumber").notEmpty().withMessage("Mobile number is required"),
+  body("firstname").trim().notEmpty().withMessage("First name is required"),
+  body("lastname").trim().notEmpty().withMessage("Last name is required"),
+  body("email").isEmail().withMessage("Valid work email is required"),
+  body("mobile").notEmpty().withMessage("Mobile number is required"),
 ];
