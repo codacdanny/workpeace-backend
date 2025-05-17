b5d5b3bc9689714ebb3a78e2d97a98b31c7217cd
Chinemerem Daniel <danpresh302@gmail.com>
2025-05-17 23:56:09 +0100

    fixed cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(helmet()); // Add security headers
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: ["http://127.0.0.1:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   })
